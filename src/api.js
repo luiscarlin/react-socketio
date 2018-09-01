73c529c931d9250c5e2d6f6bf56bff3836760d02
Luis Carlin <luiscarlin@gmail.com>
2018-09-01 08:56:37 -0400

    Add some comments

--- src/api.js
+++ src/api.js
@@ -1,8 +1,11 @@
 import openSocket from 'socket.io-client'
+
+// open connection to localhost:8080 (socket.io server)
 const  socket = openSocket('http://localhost:8000')
 
 function subscribeToTimer(cb) {
   socket.on('timer', timestamp => cb(null, timestamp))
   socket.emit('subscribeToTimer', 1000)
 }
+
 export { subscribeToTimer }
